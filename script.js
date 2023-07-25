56e2b22442475ae2a94b6140ad92f727ef73d1d3
Iwan Rahman <1w3n.rahman@gmail.com>
2023-07-25 12:09:30 -0400

    Check row for win function

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const Player = (name, sign) => {
 }
 const bob = Player("Bob","X");
 const tom = Player("Tom","O");
+
 const gameFlow = (playerOne, playerTwo) => {
   //Player one starts the game
   let activePlayer = playerOne;
@@ -17,11 +18,34 @@ const gameFlow = (playerOne, playerTwo) => {
     cell.addEventListener("click", () => {
       if(cell.textContent == ''){
         cell.textContent = activePlayer.sign;
+        
+        checkRow(activePlayer);
+
         let temp = activePlayer;
         activePlayer = inactivePlayer;
         inactivePlayer = temp;
+
       }
     });
   });
 }
+
+function checkWinner(activePlayer){};
+
+function checkRow(activePlayer){
+  for(row of document.querySelectorAll("tr")){
+    let totalRowSigns = Array.from(row.querySelectorAll("td")).reduce((total, cell) => {
+      return total += (cell.textContent == activePlayer.sign); 
+    },0)
+    
+    if(totalRowSigns == 3){
+      return true;
+    }
+  }
+
+  return false;
+};
+
+function checkColoumn(activePlayer){};
+// function checkDiagonal(){};
 
