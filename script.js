998a0734716cb840784a2f09649965a187571d57
Iwan Rahman <1w3n.rahman@gmail.com>
2023-07-25 01:30:20 -0400

    Fix bugs and update gameFlow module
    The players switch back and forth instead of only player one switching
    to player two.
    
    The active player cannot select an already selected grid.

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ const gameBoard = (() => {
 const Player = (name, sign) => {
   return {name, sign};
 }
-
+const bob = Player("Bob","X");
+const tom = Player("Tom","O");
 const gameFlow = (playerOne, playerTwo) => {
   //Player one starts the game
   let activePlayer = playerOne;
+  let inactivePlayer = playerTwo;
 
   gameBoard.forEach(cell => {
     cell.addEventListener("click", () => {
-      cell.textContent = activePlayer.sign;
-      activePlayer = playerTwo;
+      if(cell.textContent == ''){
+        cell.textContent = activePlayer.sign;
+        let temp = activePlayer;
+        activePlayer = inactivePlayer;
+        inactivePlayer = temp;
+      }
     });
   });
 }
